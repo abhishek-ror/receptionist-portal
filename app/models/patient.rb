d3b48145414142b88b01d7f3a76b0d14384a010a
Abhishek Pal <apal51640@gmail.com>
2023-07-03 16:51:26 +0530

    Fix graph of  Patient and fix local changes

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -3,9 +3,10 @@ class Patient < ApplicationRecord
 	validates :first_name, presence: true
   validates :last_name, presence: true
   validates :age, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :gender, presence: true
 
   belongs_to :user
-  
+
   def name
     "#{first_name} #{last_name}"
   end
