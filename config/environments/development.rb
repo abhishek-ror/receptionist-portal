d9394edbb66c28b333ec191578f1aab9e2dabe9d
Abhishek Pal <apal51640@gmail.com>
2023-08-16 20:02:55 +0530

    Fix Login page style

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,8 +58,8 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
-
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.hosts << "be6f-122-168-228-39.ngrok-free.app"
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
+    controllers: 'users/registrations'
   }
   
   root to: 'homes#index'
